3f3914ee803d6c78644d5a4b1ef322e250f088ad
Kekelberi <alexei.gaponov@gmail.com>
2022-02-03 18:20:38 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 // import { css,jsx, keyframes } from '@emotion/react'
-import {  css, Global, ClassNames,keyframes } from '@emotion/react'
+import { css, keyframes } from '@emotion/css'
 import './App.css'
-import logo from './logo.svg'
-import { bounceIn, bounceOut } from 'react-animations'
+import logo from './logo.svg' 
+import { bounce, merge, fadeInLeft } from 'react-animations'
 
 function App() {
-  const bounceAnimation = keyframes`${bounceIn}${bounceOut}`
+  const bounceAnimation2 = keyframes`${merge}${fadeInLeft}${bounce}`
 
+  const bounceAnimation = keyframes`${fadeInLeft}`
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          css={css`
-            animation: ${bounceAnimation} 1s ease infinite;
-          `}
-          alt='logo'
-        />
-        <div
-          css={css`
-            animation: ${bounceAnimation} 1s ease infinite;
-          `}
-        >
-          Learn React
-        </div>
-      </header>
+    <div>
+      <div className='App'>
+        <header className='App-header'>
+          <img
+            src={logo}
+            className={css`
+              height: 40vmin;
+              animation: ${bounceAnimation2} 2s infinite  alternate;
+            `}
+            alt='logo'
+          />
+          <div
+            className={css`
+              animation: ${bounceAnimation} 1s ease infinite;
+            `}
+          >
+            Learn React
+          </div>
+        </header>
+      </div>
     </div>
   )
 }
--- src/StyleTest.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const StyleTest = () => <div>
-   
-</div>
